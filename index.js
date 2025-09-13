3d36db02ff0a85b260e5a65035828b480f3db2ea
Nevin <n.boyd1@yahoo.com>
2025-09-12 18:39:52 -0700

    Ran npm test, suite passed 1/1 tests passed 4/4

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
